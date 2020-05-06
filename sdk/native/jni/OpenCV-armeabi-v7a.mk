654f14cf43c8b18199deb4f2c099448c1fe5eea6
liupingping <liupingping@fun-snap.com>
2020-05-06 19:45:35 +0800

    首次提交

---
+++ sdk/native/jni/OpenCV-armeabi-v7a.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=libjpeg libpng libtiff libjasper IlmImf
+OPENCV_EXTRA_COMPONENTS:=z D:/ffmpeg-3.4.1/android/arm/lib/libavcodec.so D:/ffmpeg-3.4.1/android/arm/lib/libavformat.so D:/ffmpeg-3.4.1/android/arm/lib/libavutil.so D:/ffmpeg-3.4.1/android/arm/lib/libswscale.so D:/ffmpeg-3.4.1/android/arm/lib/libswresample.so dl m log
